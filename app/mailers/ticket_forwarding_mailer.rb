794467e9bba7fde0302f687e92164a83b7746c1b
Caleb Woods <calebwoods@Calebs-MacBook-Pro-2.local>
2021-05-05 15:54:30 -0700

    added forwarding mailer

--- app/mailers/ticket_forwarding_mailer.rb
+++ app/mailers/ticket_forwarding_mailer.rb
@@ -3,8 +3,8 @@ class TicketForwardingMailer < ApplicationMailer
   def forwarding_ticket(ticket_id, recipient)
     @ticket = Ticket.find(ticket_id)
     @recipient = recipient
-    @printable_text = @ticket.ticket_json['NewDataSet']['tickets']['printable_text']
-    mail(to: @recipient, subject: "Forwarded ticket: #{@ticket.ticket_json['NewDataSet']['tickets']['ticket']}-#{@ticket.ticket_json['NewDataSet']['tickets']['revision']}")
+    @printable_text = @ticket.ticket['NewDataSet']['tickets']['printable_text']
+    mail(to: @recipient, subject: "Forwarded ticket: #{@ticket.ticket['NewDataSet']['tickets']['ticket']}-#{@ticket.ticket['NewDataSet']['tickets']['revision']}")
   end
 
 end
