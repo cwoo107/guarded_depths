eb3138472aac3d03b8c7923d10b91d6e8bc1e82d
Caleb Woods <calebwoods@Calebs-MacBook-Pro-2.local>
2021-05-06 11:40:36 -0700

    improved mobile layout

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -11,43 +11,6 @@ class TicketsController < ApplicationController
   def show
   end
 
-  # GET /tickets/new
-  def new
-    @ticket = Ticket.new
-  end
-
-  # GET /tickets/1/edit
-  def edit
-  end
-
-  # POST /tickets or /tickets.json
-  def create
-    @ticket = Ticket.new(ticket_params)
-
-    respond_to do |format|
-      if @ticket.save
-        format.html { redirect_to @ticket, notice: "Ticket was successfully created." }
-        format.json { render :show, status: :created, location: @ticket }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /tickets/1 or /tickets/1.json
-  def update
-    respond_to do |format|
-      if @ticket.update(ticket_params)
-        format.html { redirect_to @ticket, notice: "Ticket was successfully updated." }
-        format.json { render :show, status: :ok, location: @ticket }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /tickets/1 or /tickets/1.json
   def destroy
     @ticket.destroy
