13db5d77720912ef634f2684c441ff4d6895c4e7
Caleb Woods <calebwoods@Calebs-MacBook-Pro-2.local>
2021-05-10 11:04:09 -0700

    added hotwire

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,11 @@
 class Ticket < ApplicationRecord
+
   after_create_commit :send_ticket
+
+  after_create_commit { broadcast_prepend_to "tickets" }
+  after_update_commit { broadcast_replace_to "tickets" }
+  after_destroy_commit { broadcast_remove_to "tickets" }
+
   belongs_to :code
 
   def send_ticket
